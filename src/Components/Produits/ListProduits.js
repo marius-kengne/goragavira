e3d8f72fb128ce8705abc1b7c889de25aac3e5b1
Sofiane OMRANI <99414149+sofianeBTS@users.noreply.github.com>
2023-11-02 11:10:23 +0100

    pages name

--- src/Components/Produits/ListProduits.js
+++ src/Components/Produits/ListProduits.js
@@ -1,16 +1,28 @@
 import React, { useState } from 'react';
-import {useParams} from 'react-router-dom';
+import './Produits.css';
+import { Link } from 'react-router-dom';
 
-const ListProduits = () => {
 
-    const {id} = useParams();
+const ListProduit = () => {
+
+    // useEffect(() => {
+    //     axios.get('https://eisee-it.o3creative.fr/2023/groupe5/wp-json/wp/v2/product')
+    //       .then((response) => {
+    //         setProducts(response.data);
+    //       })
+    //       .catch((error) => {
+    //         console.error('Erreur lors de la récupération des données :', error);
+    //       });
+    //   }, []);
+
+
     const consumerKey = 'ck_e30e489bfe9990edb792ce1ad7436620dff7cb29';
     const consumerSecret = 'cs_82c3e0ccfb784baa8052e1edfbc438aa3f3724fc';
-    const [product, setProduct] = useState([]);
+    const [products, setProducts] = useState([]);
 
-    const apiUrl = 'https://eisee-it.o3creative.fr/2023/groupe5/wp-json/wc/v3/products/';
+    const apiUrl = 'https://eisee-it.o3creative.fr/2023/groupe5/wp-json/wc/v3/';
 
-    fetch(apiUrl + id, {
+    fetch(apiUrl + 'products', {
     method: 'GET',
     headers: {
         'Authorization': 'Basic ' + btoa(`${consumerKey}:${consumerSecret}`),
@@ -18,7 +30,7 @@ const ListProduits = () => {
     })
     .then(response => response.json())
     .then(data => {
-        setProduct(data);
+        setProducts(data);
         console.log(data);
     })
     .catch(error => {
@@ -31,15 +43,20 @@ const ListProduits = () => {
             <div className="filter-cont"></div>
             <div className="products-cont">
                 <div className = "products">
-                    <h2>{product.name}</h2>
-                    <img className="img"src={product.images}/> 
-                    <div dangerouslySetInnerHTML={{__html: product.description}} />
+                    {products.map((product) => (
+                        <div className='' key={product.id}>
+                            <Link to={`/product/${product.id}`}>
+                                <h2>{product.name}</h2>
+                                <p>{product.slug}</p>
+                                <img className="img"src={product.images[2].src}/>
+                                {/* <div dangerouslySetInnerHTML={{__html: product.description}} /> */}
+                            </Link>
+                        </div>
+                    ))}
                 </div>
             </div>
         </div>
     );
-
-
 };
 
-export default ListProduits;
+export default ListProduit;
