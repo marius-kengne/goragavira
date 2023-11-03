d77aa669b96a576ea5a7e14433743aebefde0fa8
Marius Harold KENGNE <haroldkengne@gmail.com>
2023-11-03 13:56:59 +0100

    filtrage des produits par catégorie

--- src/Components/Produits/ListProduits.js
+++ src/Components/Produits/ListProduits.js
@@ -1,53 +1,92 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import './Produits.css';
 import { Link } from 'react-router-dom';
 import '../Produits/Produits.css';
 import { CartContext } from '../Panier/CartContext';
 import { FaShoppingCart } from 'react-icons/fa';
 
-const ListProduit = ({ }) => {
+const ListProduit = () => {
 
     const { addToCart } = useContext(CartContext);
 
     const consumerKey = 'ck_e30e489bfe9990edb792ce1ad7436620dff7cb29';
     const consumerSecret = 'cs_82c3e0ccfb784baa8052e1edfbc438aa3f3724fc';
     const [products, setProducts] = useState([]);
+    const [category, setCategory] = useState();
+    const [searchCategory, setSearchCategory] = useState([]);
 
     const apiUrl = 'https://eisee-it.o3creative.fr/2023/groupe5/wp-json/wc/v3/';
 
-    fetch(apiUrl + 'products', {
-    method: 'GET',
-    headers: {
-        'Authorization': 'Basic ' + btoa(`${consumerKey}:${consumerSecret}`),
-    },
-    })
-    .then(response => response.json())
-    .then(data => {
-        setProducts(data);
-        console.log(data);
-    })
-    .catch(error => {
-        console.error('Erreur lors de la récupération des données :', error);
-    });
+    useEffect(() => {
+        let url = apiUrl + 'products';
 
+        if (searchCategory != '') {
+            if(searchCategory == "Volcanique"){
+                url += `?category=25`;
+            }else if(searchCategory == "Compostelle"){
+                url += `?category=22`;
+            }
+            else{
+                url += `?category=${searchCategory}`;
+            }
+            
+        }
+
+        fetch(url, {
+            method: 'GET',
+            headers: {
+                'Authorization': 'Basic ' + btoa(`${consumerKey}:${consumerSecret}`),
+            },
+        })
+        .then(response => response.json())
+        .then(data => {
+            setProducts(data);
+            console.log(data);
+        })
+        .catch(error => {
+            console.error('Erreur lors de la récupération des données :', error);
+        });
+    }, [searchCategory]);
+
+    const handleSearch = () => {
+
+        if(searchCategory == "Volcanique"){
+            setCategory(25);
+        }else if(searchCategory == "Compostelle"){
+            setCategory(22);
+        }else {
+            setCategory(searchCategory);
+        }
+        
+    };
 
     return (
         <div className='listProduits'>
-         {/* <div className="filter-cont"></div> */}
+            <div className="search">
+                <input
+                    type="text"
+                    placeholder="Rechercher par catégorie"
+                    value={searchCategory}
+                    onChange={(e) => setSearchCategory(e.target.value)}
+                />
+                <button onClick={handleSearch}>Rechercher</button>
+            </div>
             <div className="products-cont">
                 <div className = "products">
                     {products.map((product) => (
                         <div className='prod' key={product.id}>
                             <Link to={`/product/${product.id}`}>
-                                <img className="img"src={product.images[2].src}/></Link>
-                                <div className="title">
-                                   <div><Link to={`/product/${product.id}`}>  {product.name}  </Link></div>                            
-                                    <div onClick={() => addToCart(product)}><FaShoppingCart size={18} /></div>
+                                <img className="img" src={product.images[0].src} alt={product.name} />
+                            </Link>
+                            <div className="title">
+                                <div>
+                                    <Link to={`/product/${product.id}`}>{product.name}</Link>
+                                </div>                            
+                                <div onClick={() => addToCart(product)}>
+                                    <FaShoppingCart size={18} />
                                 </div>
-                           
+                            </div>
                             <div className="price">  {product.price} €</div>
-                            {/* <div dangerouslySetInnerHTML={{__html: product.description}} /> */}
-                            
                         </div>
                     ))}
                 </div>
